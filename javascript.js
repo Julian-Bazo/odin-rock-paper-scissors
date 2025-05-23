64bfd6f44341552e75513af930d95897b11f0c9d
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-05-23 10:59:08 -0400

    Create playRound()
    Creates the playRound function that takes both the human choice and computer choice to compare them. Awards a point to the winner.

--- javascript.js
+++ javascript.js
@@ -4,13 +4,13 @@ console.log("Hello WORLD!");
 function getComputerChoice() {
     let selector = Math.random();
     if (selector <= .33) {
-        return "Rock";
+        return "rock";
     }
     else if (selector <= .66 && selector > .33) {
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -29,3 +29,50 @@ function getHumanChoice() {
 //Initializes scoring mechanism integers
 let humanScore = 0;
 let computerScore = 0;
+
+
+//Allows for the playing and scoring of a round
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "You win! Rock beats scissors!";
+    }
+
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You win! Paper beats rock!";
+    }
+
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissors beats paper!";
+    }
+
+    else if (computerChoice === "rock" && humanChoice === "scissors") {
+        computerScore++;
+        return "You lose! Rock beats scissors!"
+    }
+
+    else if (computerChoice === "paper" && humanChoice === "rock") {
+        computerScore++;
+        return "You lose! Paper beats rock!"
+    }
+
+    else if (computerChoice === "scissors" && humanChoice === "paper") {
+        computerScore++;
+        return "You lose! Scissors beats paper!"
+    }
+
+    else {
+        return "It's a tie, try again!";
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+// alert(playRound(humanChoice, computerChoice));
+// alert("You: " + humanScore + " | Computer: " + computerScore);
+// console.log("Human score = " + humanScore);
+// console.log(computerScore);
