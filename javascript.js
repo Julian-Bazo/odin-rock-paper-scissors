651960aab8a502308c0a99acbfd10c400a5cf2b4
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-05-23 14:43:43 -0400

    Create playGame()
    Creates the playGame function which calks playRound 5 times.
    
    An alert tells the score after each round, with a unique final alert.

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function getComputerChoice() {
 // Returns a valid choice based off of user input
 function getHumanChoice() {
     let choice = prompt("Rock, paper, or scissors?","");
-    choice = choice.toLowerCase();
+    choice = choice.toLowerCase(choice);
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice;
     }
@@ -26,53 +26,72 @@ function getHumanChoice() {
     }
 }
 
-//Initializes scoring mechanism integers
-let humanScore = 0;
-let computerScore = 0;
 
-
-//Allows for the playing and scoring of a round
-function playRound(humanChoice, computerChoice) {
-
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++;
-        return "You win! Rock beats scissors!";
-    }
-
-    else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        return "You win! Paper beats rock!";
-    }
-
-    else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        return "You win! Scissors beats paper!";
-    }
-
-    else if (computerChoice === "rock" && humanChoice === "scissors") {
-        computerScore++;
-        return "You lose! Rock beats scissors!"
-    }
-
-    else if (computerChoice === "paper" && humanChoice === "rock") {
-        computerScore++;
-        return "You lose! Paper beats rock!"
-    }
-
-    else if (computerChoice === "scissors" && humanChoice === "paper") {
-        computerScore++;
-        return "You lose! Scissors beats paper!"
-    }
-
-    else {
-        return "It's a tie, try again!";
-    }
-}
 
 let humanChoice = getHumanChoice();
 let computerChoice = getComputerChoice();
 
-// alert(playRound(humanChoice, computerChoice));
-// alert("You: " + humanScore + " | Computer: " + computerScore);
-// console.log("Human score = " + humanScore);
-// console.log(computerScore);
+// Allows for the scoring of multiple back-to-back rounds. Ends when a score reaches 5.
+function playGame() {
+
+//Initializes scoring mechanism integers
+    let humanScore = 0;
+    let computerScore = 0;
+
+    console.log(humanChoice);
+
+    console.log(playRound(humanChoice, computerChoice));
+    alert("You: " + humanScore + " | Computer: " + computerScore);
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    alert("You: " + humanScore + " | Computer: " + computerScore);
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    alert("You: " + humanScore + " | Computer: " + computerScore);
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    alert("You: " + humanScore + " | Computer: " + computerScore);
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    alert("You: " + humanScore + " | Computer: " + computerScore);
+    alert("FINAL --> You: " + humanScore + " | Computer: " + computerScore);
+
+//Allows for the playing and scoring of a round
+    function playRound(humanChoice, computerChoice) {
+
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            return "You win! Rock beats scissors!";
+        }
+
+        else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            return "You win! Paper beats rock!";
+        }
+
+        else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            return "You win! Scissors beats paper!";
+        }
+
+        else if (computerChoice === "rock" && humanChoice === "scissors") {
+            computerScore++;
+            return "You lose! Rock beats scissors!"
+        }
+
+        else if (computerChoice === "paper" && humanChoice === "rock") {
+            computerScore++;
+            return "You lose! Paper beats rock!"
+        }
+
+        else if (computerChoice === "scissors" && humanChoice === "paper") {
+            computerScore++;
+            return "You lose! Scissors beats paper!"
+        }
+
+        else {
+            return "It's a tie, try again!";
+        }
+    }
+
+}
+
+playGame();
+// console.log(playRound(humanChoice, computerChoice));
+// console.log("You: " + humanScore + " | Computer: " + computerScore);
