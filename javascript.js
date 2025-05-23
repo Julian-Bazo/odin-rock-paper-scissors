67ff80244400253039d2b720099197326a4707de
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-05-23 10:16:54 -0400

    Create getComputerChoice
    Creates the getComputerChoice function to use Math.random() to randomly return a string of either "Rock" "Paper" or "Scissors".

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 console.log("Hello WORLD!");
 
 // Randomly will return "Rock" || "Paper" || "Scissors"
+function getComputerChoice() {
+    let selector = Math.random();
+    if (selector <= .33) {
+        return "Rock";
+    }
+    else if (selector <= .66 && selector > .33) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
